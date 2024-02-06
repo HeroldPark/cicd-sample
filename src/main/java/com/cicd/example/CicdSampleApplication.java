b128b2839a1f44e37eb044843dc5ea01209802d1
herold park <heroldpark@gmail.com>
2024-02-06 22:43:28 +0900

    2024-02-06 second commit

--- src/main/java/com/cicd/example/CicdSampleApplication.java
+++ src/main/java/com/cicd/example/CicdSampleApplication.java
@@ -1,13 +1,35 @@
 package com.cicd.example;
 
+import java.text.SimpleDateFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CicdSampleApplication {
 
-	public static void main(String[] args) {
+	static Logger logger = (Logger) LoggerFactory.getLogger(CicdSampleApplication.class);
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CicdSampleApplication.class, args);
+
+		CicdSampleApplication example = new CicdSampleApplication();
+		example.Debug(args);
 	}
+
+	public void Debug(String... args) throws Exception {
+    String currentTime = new String();
+    long now = System.currentTimeMillis();
+    currentTime = String.format("currentTime: %s", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(now));
+    logger.debug(currentTime);
+
+    logger.trace("trace -- Hello world.");
+    logger.debug("debug -- Hello world.");
+    logger.info("info -- 헬로우 월드.");
+    logger.warn("warn -- Hello world.");
+    logger.error("error -- Hello world.");
+  }
 
 }
